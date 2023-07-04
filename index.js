fcdcbd247f10ce1ead3a4e52d8cbc0d53df4b995
Aekkalak Junjongkon <111352216+JuneAekkalak@users.noreply.github.com>
2023-07-04 16:42:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const authorsRouter = require('./routes/authors');
+const articlesRouter = require('./routes/articles');
+const scraperRouter = require('./routes/scraper');
+const articlesScopusRouter = require('./routes/articlesScopus');
+const authorsScopusRouter = require('./routes/authorsScopus');
+const journalRouter = require('./routes/journalScopus');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+//mongodb+srv://root:1234@db01.uyg1g.mongodb.net/test
+// wurisdb
+mongoose.connect('mongodb+srv://root:1234@cluster0.l78dbvc.mongodb.net/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'wu-researcher'
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error(err));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors()); // Add this line to enable CORS
+app.use('/authors', authorsRouter);
+app.use('/articles', articlesRouter);
+app.use('/authorsScopus', authorsScopusRouter);
+app.use('/articlesScopus', articlesScopusRouter);
+app.use('/journals', journalRouter);
+app.use('/scraper', scraperRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
 
