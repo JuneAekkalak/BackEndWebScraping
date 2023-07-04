aea24730bc7028046f3e0fe157f944a9b59d5556
JuneAekkalak <aekkkalak.ju@gmail.com>
2023-07-04 16:53:50 +0700

    add index.js

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const portfinder = require('portfinder');
-
 
 const authorsRouter = require('./routes/authors');
 const articlesRouter = require('./routes/articles');
 const scraperRouter = require('./routes/scraper');
+const articlesScopusRouter = require('./routes/articlesScopus');
+const authorsScopusRouter = require('./routes/authorsScopus');
+const journalRouter = require('./routes/journalScopus');
 
 const app = express();
-// const databaseURI = 'mongodb+srv://root:1234@cluster0.l78dbvc.mongodb.net/test';
-const databaseURI = 'mongodb://adminwuris:wurisadmin@192.168.75.58:27017/';
-const dbName = 'wurisdb';
-
-mongoose
-  .connect(databaseURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: dbName
-  })
+const PORT = process.env.PORT || 8080;
+//mongodb+srv://root:1234@db01.uyg1g.mongodb.net/test
+// wu-researcher
+mongoose.connect('mongodb+srv://root:1234@cluster0.l78dbvc.mongodb.net/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'wurisdb'
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error(err));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors()); // Add this line to enable CORS
 app.use('/authors', authorsRouter);
 app.use('/articles', articlesRouter);
+app.use('/authorsScopus', authorsScopusRouter);
+app.use('/articlesScopus', articlesScopusRouter);
+app.use('/journals', journalRouter);
 app.use('/scraper', scraperRouter);
 
-
-portfinder.getPort((err, port) => {
-  if (err) {
-    console.error(err);
-  } else {
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  }
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
+
