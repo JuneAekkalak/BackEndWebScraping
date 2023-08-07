1cb0321aa5476b2f462daedfc04d6d4e1c2dcc8e
Aekkalak Junjongkon <111352216+JuneAekkalak@users.noreply.github.com>
2023-08-07 15:49:08 +0700

    Update connectToMongoDB.js

--- qurey/connectToMongoDB.js
+++ qurey/connectToMongoDB.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
-  const databaseURI = 'mongodb://adminwuris:wurisadmin@192.168.75.58:27017/';
-  const dbName = 'wuris';
+  const databaseURI = 'mongodb+srv://root:1234@cluster0.l78dbvc.mongodb.net/test';
+  const dbName = 'wu-researcher';
   try {
     await mongoose.connect(databaseURI, {
       useNewUrlParser: true,
